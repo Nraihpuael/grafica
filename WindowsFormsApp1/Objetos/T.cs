5ce17dc105f363a8e8183fde6d888c3b00dd5274
Nraihpuael <nahuelripa@hotmail.com>
2024-08-31 02:28:06 -0400

    tarea 2

--- WindowsFormsApp1/Objetos/T.cs
+++ WindowsFormsApp1/Objetos/T.cs
@@ -95,66 +95,78 @@ namespace WindowsFormsApp1.Objetos
 
         int[] indices = new int[]
         {
-        // Vertical part of the "T"
-        // Front face
-        0, 1, 2,
-        2, 3, 0,
+            // Vertical part of the "T"
+            // Front face
+            0, 1, 2,
+            2, 3, 0,
 
-        // Back face
-        4, 5, 6,
-        6, 7, 4,
+            // Back face
+            4, 5, 6,
+            6, 7, 4,
 
-        // Top face
-        8, 9, 10,
-        10, 11, 8,
+            // Top face
+            8, 9, 10,
+            10, 11, 8,
 
-        // Bottom face
-        12, 13, 14,
-        14, 15, 12,
+            // Bottom face
+            12, 13, 14,
+            14, 15, 12,
 
-        // Left face
-        16, 17, 18,
-        18, 19, 16,
+            // Left face
+            16, 17, 18,
+            18, 19, 16,
 
-        // Right face
-        20, 21, 22,
-        22, 23, 20,
+            // Right face
+            20, 21, 22,
+            22, 23, 20,
 
-        // Horizontal part of the "T"
-        // Front face
-        24, 25, 26,
-        26, 27, 24,
+            // Horizontal part of the "T"
+            // Front face
+            24, 25, 26,
+            26, 27, 24,
 
-        // Back face
-        28, 29, 30,
-        30, 31, 28,
+            // Back face
+            28, 29, 30,
+            30, 31, 28,
 
-        // Top face
-        32, 33, 34,
-        34, 35, 32,
+            // Top face
+            32, 33, 34,
+            34, 35, 32,
 
-        // Bottom face
-        36, 37, 38,
-        38, 39, 36,
+            // Bottom face
+            36, 37, 38,
+            38, 39, 36,
 
-        // Left face
-        40, 41, 42,
-        42, 43, 40,
+            // Left face
+            40, 41, 42,
+            42, 43, 40,
 
-        // Right face
-        44, 45, 46,
-        46, 47, 44
+            // Right face
+            44, 45, 46,
+            46, 47, 44
         };
 
-        public void Dibujar()
-        {      
+        public void Dibujar(Vector3 centroDeMasa)
+        {
+
+           float[] vertices2 = new float[vertices.Length];
+
+            for (int i = 0; i < vertices.Length; i += 6)
+            {
+                vertices2[i] = vertices[i] + centroDeMasa.X;
+                vertices2[i + 1] = vertices[i + 1] + centroDeMasa.Y;
+                vertices2[i + 2] = vertices[i + 2] + centroDeMasa.Z;
+                vertices2[i + 3] = vertices[i + 3];
+                vertices2[i + 4] = vertices[i + 4];
+                vertices2[i + 5] = vertices[i + 5];
+            }
 
             VertexArrayObject = GL.GenVertexArray();
             GL.BindVertexArray(VertexArrayObject);
 
             VertexBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices2.Length * sizeof(float), vertices2, BufferUsageHint.StaticDraw);
 
             ElementBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
@@ -166,7 +178,11 @@ namespace WindowsFormsApp1.Objetos
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
             GL.EnableVertexAttribArray(1);
 
+
+
             GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            GL.BindVertexArray(0);
+
         }
 
     }
